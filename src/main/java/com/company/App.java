d22fa5a29a6c138c04769e860be3d052e658723e
jason <jason>
2022-03-29 23:42:50 -0700

    added movie card

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,6 +1,16 @@
 package com.company;
 
 public class App {
+    static String addSpaces(int size) {
+        String result = "";
+        int num = 40 - size+6;
+        for (int i = 0; i < num; i++) {
+            result += " ";
+        }
+        result += "*";
+
+        return result;
+    }
     public static void main(String[] args) {
         Person person1 = new Person("John Smith", 75000, 10000);
         Person person2 = new Person("Jane Doe", 75000, 20000);
@@ -10,8 +20,8 @@ public class App {
         Actor maleHero = new Actor("Robert Pattinson", 3000000, 10000000,"Batman");
         Actor femaleHero = new Actor("Zoe Kravitz", 2000000, 5000000, "Catwoman");
         Actor leadVillain = new Actor("Paul Dano", 1000000, 1000000, "Riddler");
-        Actor supportingVillain = new Actor("Robert Pattinson", 500000, "Penguin");
-        Actor leadDetective = new Actor("Jeffrey Wright", 500000, "Someone");
+        Actor supportingVillain = new Actor("Collin Farrell", 500000, "Penguin");
+        Actor leadDetective = new Actor("Jeffrey Wright", 500000, "Detective");
 
         Crew c1 = new Crew("Kat Ali", 200000, 10000, "Makeup");
         Crew c2 = new Crew("Sally Abbott", 100000, 5000, "Art");
@@ -52,5 +62,43 @@ public class App {
         System.out.println("Movie Profits: " + theBatmanMovie.profit);
         System.out.println("Director Royalties: " + director.royalty);
 
+        System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        System.out.println("*                                                     *");
+        System.out.println("*                       MOVIE                         *");
+        System.out.println("*                     " + theBatmanMovie.title + "                      *");
+        System.out.println("*                                                     *");
+        System.out.println("*    Genre: " + theBatmanMovie.genre + "              Length: " + theBatmanMovie.length + "        *");
+        System.out.println("*                                                     *");
+        System.out.println("*                      Starring                       *");
+        System.out.println("*    " + maleHero.name + "     &     " + femaleHero.name +"           *");
+        System.out.println("*                                                     *");
+        System.out.println("*                     Directed By                     *");
+        System.out.println("*                     " + director.name + "                     *");
+        System.out.println("*                                                     *");
+        System.out.println("*        Actors                                       *");
+        for(Actor actor: theBatmanMovie.actors){
+            System.out.println("*    " + actor.name + " - " + actor.role + addSpaces(actor.name.length() + actor.role.length()));
+        }
+        System.out.println("*                                                     *");
+        System.out.println("*          Crew                                       *");
+        for(Crew crew: theBatmanMovie.crew){
+            System.out.println("*    " + crew.name + " - " + crew.department + addSpaces(crew.name.length() + crew.department.length()));
+        }
+        System.out.println("*                                                     *");
+        System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        System.out.println("");
+        System.out.println("                        INFO                          ");
+        System.out.println("PA1 Earnings: " + p1.salary);
+        System.out.println("PA2 Earnings: " + p2.salary);
+        System.out.println("Budget: " + theBatmanMovie.budget);
+        System.out.println("Money Earned: " + theBatmanMovie.moneyEarned);
+        System.out.println("Money Spent: " + theBatmanMovie.moneySpent);
+        System.out.println("Profits: " + theBatmanMovie.profit);
+        System.out.println("Director Royalties: " + director.royalty);
+//        System.out.println("*                                                     *");
+//
+//        System.out.println("*                                                     *");
+//        System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * *");
     }
+
 }
